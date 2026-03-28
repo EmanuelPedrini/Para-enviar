3377d7f99104be291e04d24d7e7caf5fd6ea85ef
EmanuelPedrini <mistydorydory@gmail.com>
2026-03-27 22:15:58 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,35 @@
+# nome=input("Escreva seu nome")
+# print(nome)
+
+# num=int(input("Digite um número"))
+# print(f"{num-1}, {num}, {num+1}")
+
+# print (f"{num:.2f}")
+
+# idade=input("idade")
+# print(f"Sua idade é {idade}")
+
+# alt=input("altura")
+# print(f"{nome}\n {idade}\n {alt}")
+
+# palavra=input("digite uma palavra")
+# print(len(palavra))
+
+# LETRA=input("digite uma letra")
+# print(LETRA.upper())
+# print(LETRA.lower())
+
+# numeros=input("Digite 0 e 1")
+# for num in numeros:
+#     if num=="0":
+#         print("False")
+#     if num=="1":
+#         print("True")
+
+# texto1=input("texto1")
+# texto2=input("texto2")
+# print(texto1 + texto2)
+
+# nomecid=input("nome")
+# numhab=input("habitantes")
+# print(f"A cidade de {nomecid} tem cerca de {numhab} habitantes!")
---
+++ 02.py
@@ -0,0 +1,10 @@
+
+# num1=float(input("n1"))
+# num2=float(input("n2"))
+# num3=float(input("n3"))
+# print(f"Soma:{num1+num2}")
+# print(f"Sub:{num1-num2}")
+# print(f"Mult:{num1*num2}")
+# print(f"Div: {num1/num2}")
+# print(f"Média: {(num1+num2+num3)/3}")
+metros=0
